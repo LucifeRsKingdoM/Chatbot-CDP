10d2ece1f6935cd71d8e14d135639aabf2c0ee45
Yogesh  .V <156904450+LucifeRsKingdoM@users.noreply.github.com>
2025-03-10 16:53:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,10 +28,9 @@ db.connect((err) => {
 
 // Ensure Users Table Exists
 db.query(
-  `CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+  `CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   )`,
